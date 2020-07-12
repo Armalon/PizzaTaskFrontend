18e44075ef59ca3d26361caa22ff24ad954b8c28
Armalon <armalong@gmail.com>
2020-07-12 11:53:11 +0200

    Chat templates

---
+++ frontend/chat/src/components/ChatMessage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+<!--        <div class="media media-meta-day">Today</div>-->
+<!--        media-chat-reverse CLASS -->
+        <div class="media media-chat">
+            <div class="media-body">
+                <p>Hiii, I'm good.</p>
+                <p>How are you doing?</p>
+                <p>Long time no see! Tomorrow office. will be free on sunday.</p>
+                <p class="meta"><time datetime="2018">00:06</time></p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style></style>
