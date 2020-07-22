b72ade18c64dbc5c4e3b816b85ef0cf1c0e51a48
Armalon <armalong@gmail.com>
2020-07-22 16:52:42 +0200

    Login component added

---
+++ frontend/pizza/src/components/Login.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <a class="btn btn-outline-primary my-2" href="#">Sign in</a>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
--- frontend/pizza/src/components/Navigation.vue
+++ frontend/pizza/src/components/Navigation.vue
@@ -2,8 +2,6 @@
     <nav class="my-2 my-md-0 mr-md-3">
         <router-link class="p-2 text-dark" :to="{ name: 'home' }">Menu</router-link>
         <router-link class="p-2 text-dark" :to="{ name: 'contacts' }">Contacts</router-link>
-
-        <a class="btn btn-outline-primary my-2" href="#">Sign in</a>
     </nav>
 </template>
 
