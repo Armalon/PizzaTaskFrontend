edd51ab134de3a18e8f82bf1b639bd42c2f236a5
Armalon <armalong@gmail.com>
2020-07-24 11:23:21 +0200

    My orders link in Nav menu

--- frontend/pizza/src/components/Navigation.vue
+++ frontend/pizza/src/components/Navigation.vue
@@ -1,6 +1,7 @@
 <template>
-    <nav class="my-2 my-md-0 mr-md-3">
+    <nav class="navbar navbar-light bg-light navbar-brand px-2 py-4 my-2 my-md-0 mr-md-3">
         <router-link class="p-2 text-dark" :to="{ name: 'home' }">Menu</router-link>
+        <router-link v-if="iAmAuthorized" class="p-2 text-dark text-nowrap" :to="{ name: 'orders' }">My orders</router-link>
         <router-link class="p-2 text-dark" :to="{ name: 'contacts' }">Contacts</router-link>
     </nav>
 </template>
